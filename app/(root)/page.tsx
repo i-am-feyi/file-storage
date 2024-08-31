a5cecdd750f8fce7c2280f54bc854e843278b02e
Feyisayo Badmus <feymus99@gmail.com>
2024-08-31 17:27:58 +0300

    chore: minimal landing page design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,48 @@
-import { SignInButton } from "@clerk/nextjs";
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
-    <div>
-      <SignInButton />
-    </div>
+    <section className="py-48">
+      <div className="container ">
+        <div className="grid items-center text-center max-w-2xl mx-auto">
+          <h1 className="text-6xl font-semibold tracking-tighter">
+            Welcome to the new age of cloud storage.
+          </h1>
+          <p className="text-xl text-black/50 mt-6">
+            Effortlessly secure your files in the cloud and access them from anywhere,
+            anytime. Get started for free.
+          </p>
+        </div>
+        <div className="flex items-center justify-center gap-8 mt-12">
+          <Button size="lg" className=" font-semibold" asChild>
+            <Link href="/sign-up" className="inline-flex gap-2">
+              <span>Get started</span>
+              <ArrowRight className="size-4" />
+            </Link>
+          </Button>
+          <Button
+            variant="ghost"
+            size="lg"
+            className="bg-slate-100 font-semibold text-black/80"
+          >
+            Learn more
+          </Button>
+        </div>
+
+        <div className="py-16 flex justify-center">
+          <Image
+            src="/images/dashboard.png"
+            alt="dashboard"
+            width={1200}
+            height={100}
+            className="rounded-xl md:rounded-[3rem] lg:rounded-[5rem]"
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,19 @@
 "use client";
 
-import { OrganizationSwitcher, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  OrganizationSwitcher,
+  SignedIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 import { Authenticated, Unauthenticated } from "convex/react";
 import React from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { Loader2 } from "lucide-react";
 
 const Header = () => {
   return (
@@ -19,10 +28,29 @@ const Header = () => {
               height={100}
             />
           </div>
-          <div className="flex items-center gap-4">
-            <OrganizationSwitcher />
-            <UserButton />
-          </div>
+          <ClerkLoading>
+            <Loader2 className="animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <Authenticated>
+              <div className="flex items-center gap-4">
+                <OrganizationSwitcher />
+                <UserButton />
+              </div>
+            </Authenticated>
+            <Unauthenticated>
+              <div className="flex gap-4">
+                <Button variant="ghost" asChild>
+                  <Link href="/sign-in" className="bg-slate-200">
+                    Sign in
+                  </Link>
+                </Button>
+                {/* <Button asChild>
+                  <Link href="/sign-up">Get started</Link>
+                </Button> */}
+              </div>
+            </Unauthenticated>
+          </ClerkLoaded>
         </div>
       </div>
     </div>
